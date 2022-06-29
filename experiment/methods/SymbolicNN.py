7b4e50eddecc113da69e321635859170ff05b0d9
Nisal Ranasinghe <nsranasinghe@student.unimelb.edu.au>
2022-06-29 14:15:52 +1000

    add complexity function to symbolic NN

--- experiment/methods/SymbolicNN.py
+++ experiment/methods/SymbolicNN.py
@@ -1,4 +1,5 @@
 from .src.symNN.sym_nn import SymNN
+from sympy import preorder_traversal
 
 hyper_params = {'reg_change':0.3,
                 'start_ln_block':1,
@@ -16,7 +17,18 @@ est = SymNN(reg_change=0.3,
             num_epochs=200,
             freeze=False)
 
+def eq_complexity(expr):
+    c = 0
+    for arg in preorder_traversal(expr):
+        c += 1
+    return c
 
 def model(est):
     return est.recovered_eq
+
+
+def complexity(est):
+    eq = est.recovered_eq
+    return eq_complexity(eq)
+
 
